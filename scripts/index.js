388a1d714d131114308eb7c946206dced1e96d04
cbarboza2011 <cbarboza2011@gmail.com>
2019-12-10 13:09:55 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
     });
   });
   $('.grid-box.graphic').on('click', function() {
-    var $imgUrl = $(this).css('background-image').replace('url("file:///C:/Users/Cristiana/Desktop/cbarboza2011.github.io/', '').replace('https://cristianabarboza.netlify.com/url(%22https://cristianabarboza.netlify.com/', '').replace('")', '');
+    var $imgUrl = $(this).css('background-image').replace('url("file:///C:/Users/Cristiana/Desktop/cbarboza2011.github.io/', '').replace('")', '');
+    console.log($imgUrl);
     $('.big-photo').show();
     $('.enlarged').attr('src', $imgUrl);
     $('.exit').on('click', function() {

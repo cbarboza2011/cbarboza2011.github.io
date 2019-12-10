0e4137b44062348136570f4bc4075294b2a0e42e
cbarboza2011 <cbarboza2011@gmail.com>
2019-12-10 12:44:01 -0500

    add photography and work experience

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+  $('.photo-column div').on('click', function() {
+    var $img = $(this).find('img');
+    var $imgUrl = $img.attr('src');
+    $('.big-photo').show();
+    $('.enlarged').attr('src', $imgUrl);
+    $('.exit').on('click', function() {
+      $('.big-photo').hide();
+    });
+  });
+  $('.grid-box.graphic').on('click', function() {
+    var $imgUrl = $(this).css('background-image').replace('url("file:///C:/Users/Cristiana/Desktop/cbarboza2011.github.io/', '').replace('")', '');
+    $('.big-photo').show();
+    $('.enlarged').attr('src', $imgUrl);
+    $('.exit').on('click', function() {
+      $('.big-photo').hide();
+    });
+  });
 });

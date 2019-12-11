6af62da86c20370492ca417dca895c278e86ae49
cbarboza2011 <cbarboza2011@gmail.com>
2019-12-11 15:21:24 -0500

    mobile updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  $('.navigation').hide();
+  const $portrait = $('.headshot').detach();
+  $('#main').prepend($portrait);
   $('.photo-column div').on('click', function() {
     var $img = $(this).find('img');
     var $imgUrl = $img.attr('src');
@@ -8,6 +11,7 @@ $(document).ready(function() {
       $('.big-photo').hide();
     });
   });
+
   $('.grid-box.graphic').on('click', function() {
     var $imgUrl = $(this).css('background-image').replace('url("', '').replace('")', '');
     console.log($imgUrl);
@@ -17,4 +21,26 @@ $(document).ready(function() {
       $('.big-photo').hide();
     });
   });
+
+  $('.navcontrol').on('click', function() {
+    if ($('.navigation').is(':visible')) {
+      $('.navigation').slideToggle();
+      $('.navcontrol').text('☰');
+    } else {
+      $('.navigation').slideToggle();
+      $('.navcontrol').text('×');
+    }
+  });
+
+  var x = window.matchMedia("(max-width: 700px)");
+  checkIfMobile(x) // Call listener function at run time
+  x.addListener(checkIfMobile) // Attach listener function on state changes
+
+  function checkIfMobile(x) {
+    if (x.matches) { // if mobile
+      $('#photography .subheader').text('Click for full screen');
+    }
+  }
+
+
 });
